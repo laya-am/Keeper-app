54c300bbaf31f4be70a8800ed9d6e476b72eff6e
Laya Aminosharei <laya.aminosharei@gmail.com>
2023-02-06 00:17:50 +0100

    Updates

---
+++ precache-manifest.48630840b4906b1bd0b37c2a076f006e.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31b5941b8c198edf6160e6c231376c05",
+    "url": "/Keeper-app/index.html"
+  },
+  {
+    "revision": "ada3ff822b436437b81d",
+    "url": "/Keeper-app/static/js/2.5b57fbba.chunk.js"
+  },
+  {
+    "revision": "d96293d7491fa111ae86",
+    "url": "/Keeper-app/static/js/main.e117e50a.chunk.js"
+  },
+  {
+    "revision": "73648d98793203f0dc48",
+    "url": "/Keeper-app/static/js/runtime-main.7d9478be.js"
+  }
+]);
